6cc6f795ff502b2607e32e21914957e1d1d381f5
Sergey-Spodarev <spodarev8@gmail.com>
2025-02-25 20:10:24 +0300

    creating functions in myList to delete

--- src/main/java/collections/MyList.java
+++ src/main/java/collections/MyList.java
@@ -41,4 +41,64 @@ public class MyList<T> {
         }
         return slider.value == value ? slider.value : null;
     }
+
+    public T getByIndex(int index){
+        if (head == null && index > size){
+            return null;
+        }
+        Node<T> slider = head;
+        for (int i = 0; i < index; ++i){
+            slider = slider.next;
+        }
+        return slider.value;
+    }
+
+    public void delLastNode() {
+        if (head == null) {
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            size = 0;
+            return;
+        }
+        Node<T> slider = head;
+        while (slider.next.next != null) {
+            slider = slider.next;
+        }
+        slider.next = null;
+        size--;
+    }
+
+    public void delFirstNode(){
+        if (head == null) {
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            size = 0;
+            return;
+        }
+        Node<T> newHead = head.next;
+        head = null;
+        head = newHead;
+        size--;
+    }
+
+    public void delByIndex(int index){
+        if (head == null && index > size){
+            return;
+        }
+        Node<T> slider = head;
+        for (int i = 0; i < index - 1; ++i){
+            slider = slider.next;
+        }
+        if (index < size - 2){
+            slider.next = slider.next.next;
+        }
+        else{
+            slider.next = null;
+        }
+        size--;
+    }
 }
